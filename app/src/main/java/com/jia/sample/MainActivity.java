55059ebd658b5800e69cacfd30ec6c84f3944c45
shuaijia <819418850@qq.com>
2018-01-25 16:07:57 +0800

    jia

--- app/src/main/java/com/jia/sample/MainActivity.java
+++ app/src/main/java/com/jia/sample/MainActivity.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
